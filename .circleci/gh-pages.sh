9e25e051b05648951cad767d907b768aae5e498d
Masaya Kazama <miyan@t-p.jp>
2018-01-08 19:44:28 +0900

    Add deploy script

---
+++ .circleci/gh-pages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ./dist
+git init
+git add .
+git -c user.name=$GITHUB_USER_NAME -c user.email=$GITHUB_USER_EMAIL commit -m "Auto Deployment"
+git push -f -q https://$GITHUB_USER_NAME:$GITHUB_API_KEY@github.com/$GITHUB_USER_NAME/$GITHUB_USER_NAME.github.io master
