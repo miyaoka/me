c991e7d149fe52f69f98f84d10912f56ab897641
Masaya Kazama <miyan@t-p.jp>
2018-01-31 23:09:06 +0900

    Change env name

--- .circleci/gh-pages.sh
+++ .circleci/gh-pages.sh
@@ -3,4 +3,4 @@ cd ./dist
 git init
 git add .
 git -c user.name=$GITHUB_USER_NAME -c user.email=$GITHUB_USER_EMAIL commit -m "Auto Deployment"
-git push -f -q https://$GITHUB_USER_NAME:$GITHUB_API_KEY@github.com/$GITHUB_USER_NAME/$GITHUB_USER_NAME.github.io master
+git push -f -q https://$GITHUB_USER_NAME:$GITHUB_ACCESS_TOKEN@github.com/$GITHUB_USER_NAME/$GITHUB_USER_NAME.github.io master
