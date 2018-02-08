d150e057883577233f725cfa7a26e293395b6d60
Masaya Kazama <miyan@t-p.jp>
2018-02-08 23:45:03 +0900

    Create header, routes comps

---
+++ components/AppHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="header">
+    <div v-html="about"/>
+    <div v-html="accounts"/>
+  </header>
+</template>
+
+<script>
+import about from '~/content/about.md'
+import accounts from '~/content/accounts.md'
+
+export default {
+  data() {
+    return {
+      about,
+      accounts
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~assets/css/mixin/_mediaquery.scss';
+
+$side-width: 400px;
+$clr-main: #e3cf7f;
+$clr-sub: #3c4271;
+
+.header {
+  // position: fixed;
+  width: $side-width;
+  padding: 2rem;
+  color: $clr-main;
+  overflow: auto;
+  height: 100%;
+
+  @include mq(tablet) {
+    position: unset;
+    width: auto;
+    padding: 1rem;
+  }
+
+  a {
+    color: $clr-main;
+    text-decoration: underline;
+    &:hover {
+      color: $clr-sub;
+      background: $clr-main;
+    }
+  }
+}
+</style>
---
+++ components/AppRoutes.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="routes">
+    <nuxt-link to="/">読み物</nuxt-link>
+    <nuxt-link to="/activity">活動</nuxt-link>
+    <nuxt-link to="/works">つくってみた系</nuxt-link>
+    <nuxt-link to="/keyboard">キーボード</nuxt-link>
+    <nuxt-link to="/career">経歴</nuxt-link>
+    <nuxt-link to="/contact">コンタクト</nuxt-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/css/mixin/_mediaquery.scss';
+.routes {
+  margin: 1rem 0;
+
+  & > * {
+    margin-right: 1rem;
+  }
+}
+</style>
